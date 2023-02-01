6eb31934dfd937b5eb4e2996849a9a7f3de39ca4
Gregory Pounds <greg.pounds3@gmail.com>
2023-02-01 16:23:25 -0600

    First commit as test

---
+++ index.js
@@ -0,0 +1,11 @@
+// let button = document.getElementById('btn')
+// button.style.position = 'absolute';
+// button.addEventListener('click', function(event) {
+//     console.log("I've been clicked!", event)
+//     // Anything you want to do goes here
+//     document.body.style.backgroundColor = 'lightblue'
+
+//     let p = document.createElement('p')
+//     p.innerText = 'Hello, I am new.'
+//     document.body.appendChild(p)
+// })
